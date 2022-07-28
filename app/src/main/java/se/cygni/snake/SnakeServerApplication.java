917bd3454bf2bb3ed88a023b46e3c0993d697f27
Sebastian <61822376+SebastianH-ctrl@users.noreply.github.com>
2022-07-28 14:28:33 +0200

    Enabled scheduling for removing games when passing limit

--- app/src/main/java/se/cygni/snake/SnakeServerApplication.java
+++ app/src/main/java/se/cygni/snake/SnakeServerApplication.java
@@ -5,18 +5,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableWebSocket
+@EnableScheduling
 public class SnakeServerApplication {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-	// If no active profile is set, default to development!
-	if (StringUtils.isEmpty(System.getProperty("spring.profiles.active"))) {
-	    System.setProperty("spring.profiles.active", "development");
+		// If no active profile is set, default to development!
+		if (StringUtils.isEmpty(System.getProperty("spring.profiles.active"))) {
+			System.setProperty("spring.profiles.active", "development");
+		}
+
+		SpringApplication.run(SnakeServerApplication.class, args);
 	}
-
-	SpringApplication.run(SnakeServerApplication.class, args);
-    }
 }
